52ed0dd546ee72cce3edb3992e3c99e7356822e3
GC-Kairos <113116204+GC-Kairos@users.noreply.github.com>
2022-12-01 08:42:47 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,152 @@
+navigator.serviceWorker.register('sw.js');
+const btnSave = document.querySelector('#btn-save');
+const textArea = document.querySelector('#text-1');
+const container = document.querySelector('.collection');
+let aNotas = [];
+
+
+// CLASS
+
+class Nota {
+  constructor(notas, horas) {
+    this.notas = notas;
+    this.horas = horas;
+  }
+
+
+  set setNota(notas) {
+    this.notas = notas;
+  }
+
+  get getNota() {
+    return this.notas;
+  }
+
+  set setHora(horas) {
+    this.horas = horas;
+  }
+
+  get getHora() {
+    return this.horas;
+  }
+
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  let sideNav = document.querySelectorAll('.sidenav');
+  let instanciaSide = M.Sidenav.init(sideNav, {});
+
+  let modal = document.querySelectorAll('.modal');
+  let instanciaModal = M.Modal.init(modal, {});
+
+  leerNotas();
+  renderizarNotas(aNotas);
+});
+
+/* - FUNCION 1:  Obtiene el texto del textArea y guarda en el texto en el array - */
+btnSave.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  let nota = document.querySelector('#text-1').value;
+  let cHora = new Date();
+  let hora = cHora.toLocaleString();
+  let notas = new Nota(nota, hora);
+
+  if (!nota == "") {
+
+    aNotas.push(notas);
+
+    localStorage.setItem("tasks", JSON.stringify(aNotas));
+
+    window.location.reload()
+
+  }
+
+
+})
+
+
+
+function leerNotas() {
+
+  let traerNotas = JSON.parse(localStorage.getItem("tasks"));
+
+  if (traerNotas) {
+    aNotas = traerNotas;
+  } else {
+    aNotas = [];
+  }
+
+
+  return aNotas;
+}
+
+function renderizarNotas(aNotas) {
+
+
+  leerNotas();
+
+  if (aNotas.length === 0) {
+    container.innerHTML = `
+    <p class="alert">No tienes tarea pendientes en este momento</p>
+  `;
+
+  }
+  else {
+    container.innerHTML = "";
+  }
+
+
+  for (x of aNotas) {
+
+    li = document.createElement('li');
+    li.setAttribute('class', 'notas');
+    container.appendChild(li);
+
+
+    let p1 = document.createElement('p');
+    li.append(p1);
+    p1.setAttribute('class', 'hora');
+    p1.textContent = x.horas;
+
+    let p2 = document.createElement('p');
+    p1.after(p2);
+    p2.textContent = x.notas;
+
+    let btn = document.createElement('button');
+    p2.after(btn);
+    btn.setAttribute('class', 'del');
+
+    let btnicon = document.createElement('i');
+    btn.append(btnicon);
+    btn.append(btnicon);
+    btnicon.setAttribute('class', 'material-icons delete');
+    btnicon.textContent = 'delete'
+
+
+    const del = document.querySelector('.del');
+
+
+    del.addEventListener('click', (e) => {
+
+      for (x of aNotas) {
+        let resultado = aNotas.findIndex((x) => x == aNotas.notas)
+        aNotas.splice(resultado, 1);
+
+      }
+     
+
+      localStorage.setItem("tasks", JSON.stringify(aNotas));
+
+      window.location.reload()
+    })
+
+
+  }
+
+}
+
+leerNotas();
+
+renderizarNotas(aNotas);
---
+++ sw.js
@@ -0,0 +1,57 @@
+const CACHE_NAME = 'mi-cache-1';
+const CACHE_ESTABLE = 'mi-cache-2';
+
+
+self.addEventListener('install', evento => {
+    const respCache = caches.open(CACHE_NAME).then( cache => {
+        return cache.addAll([
+            '/',
+            'index.html',
+            'css/estilos.css',
+            'app.js',
+          
+            
+        ])
+    })
+
+    evento.waitUntil(respCache);   
+})
+
+self.addEventListener('fetch', evento => {
+    const respuesta = fetch(  evento.request ).then( respNet => {
+        return caches.open(CACHE_NAME).then( cache => {
+            cache.put( evento.request, respNet.clone() );
+            return respNet;
+        })
+    }).catch( error => {  
+        return caches.match(evento.request);
+    })
+    evento.respondWith(respuesta);
+})
+
+
+self.addEventListener('install', evento => {
+    const respCache = caches.open(CACHE_ESTABLE).then( cache => {
+        return cache.addAll([
+                     
+            'icons/icon-96x96.png',
+            'https://fonts.googleapis.com/icon?family=Material+Icons',
+            'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js',
+            
+        ])
+    })
+
+    evento.waitUntil(respCache);   
+})
+
+self.addEventListener('fetch', evento => {
+    const respuesta = fetch(  evento.request ).then( respNet => {
+        return caches.open(CACHE_ESTABLE).then( cache => {
+            cache.put( evento.request, respNet.clone() );
+            return respNet;
+        })
+    }).catch( error => {  
+        return caches.match(evento.request);
+    })
+    evento.respondWith(respuesta);
+})
