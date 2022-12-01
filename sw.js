824030798a1ed008fd0b30f6f17311ca40c71de7
GC-Kairos <113116204+GC-Kairos@users.noreply.github.com>
2022-12-01 09:55:24 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,22 +3,42 @@ const CACHE_ESTABLE = 'mi-cache-2';
 
 
 self.addEventListener('install', evento => {
-    const respCache = caches.open(CACHE_NAME).then( cache => {
-        return cache.addAll([
-            './',
-            './index.html',
-            './css/estilos.css',
-            './app.js',
-            './icons/icon-96x96.png',
-            'https://fonts.googleapis.com/icon?family=Material+Icons',
-            'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js'
-            
-          
-            
-        ])
-    })
 
-    evento.waitUntil(respCache);   
+    evento.waitUntil(
+        
+        (async() => {
+            try {
+                cache_obj = await caches.open(CACHE_NAME)
+                cache_obj.addAll(
+
+                    './',
+                    './index.html',
+                    './css/estilos.css',
+                    './app.js',
+                    './icons/icon-96x96.png',
+                    'https://fonts.googleapis.com/icon?family=Material+Icons',
+                    'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js'
+                    )
+                 }catch {
+                console.log("Error en el cacheo")
+                }
+       })
+                    )
+    
+    // const respCache = caches.open(CACHE_NAME).then( cache => {
+    //     return cache.addAll([
+    //         './',
+    //         './index.html',
+    //         './css/estilos.css',
+    //         './app.js',
+    //         './icons/icon-96x96.png',
+    //         'https://fonts.googleapis.com/icon?family=Material+Icons',
+    //         'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js'
+                      
+    //     ])
+    // })
+
+    // evento.waitUntil(respCache);   
 })
 
 self.addEventListener('fetch', evento => {
