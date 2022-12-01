1573d510c283bc2c88495507f4928a80a55c4fe9
GC-Kairos <113116204+GC-Kairos@users.noreply.github.com>
2022-12-01 09:59:36 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,4 @@
 const CACHE_NAME = 'mi-cache-1';
-const CACHE_ESTABLE = 'mi-cache-2';
-
 
 self.addEventListener('install', evento => {
 
@@ -23,23 +21,8 @@ self.addEventListener('install', evento => {
                 console.log("Error en el cacheo")
                 }
        })
-                    )
-    
-    // const respCache = caches.open(CACHE_NAME).then( cache => {
-    //     return cache.addAll([
-    //         './',
-    //         './index.html',
-    //         './css/estilos.css',
-    //         './app.js',
-    //         './icons/icon-96x96.png',
-    //         'https://fonts.googleapis.com/icon?family=Material+Icons',
-    //         'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js'
-                      
-    //     ])
-    // })
-
-    // evento.waitUntil(respCache);   
-})
+                   )
+  })
 
 self.addEventListener('fetch', evento => {
     const respuesta = fetch(  evento.request ).then( respNet => {
@@ -53,29 +36,3 @@ self.addEventListener('fetch', evento => {
     evento.respondWith(respuesta);
 })
 
-
-// self.addEventListener('install', evento => {
-//     const respCache = caches.open(CACHE_ESTABLE).then( cache2 => {
-//         return cache2.addAll([
-//             '/',
-//             'icons/icon-96x96.png',
-//             'https://fonts.googleapis.com/icon?family=Material+Icons',
-//             'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js'
-            
-//         ])
-//     })
-
-//     evento.waitUntil(respCache);   
-// })
-
-// self.addEventListener('fetch', evento => {
-//     const respuesta = fetch(  evento.request ).then( respNet => {
-//         return caches.open(CACHE_ESTABLE).then( cache2 => {
-//             cache2.put( evento.request, respNet.clone() );
-//             return respNet;
-//         })
-//     }).catch( error => {  
-//         return caches.match(evento.request);
-//     })
-//     evento.respondWith(respuesta);
-// })
