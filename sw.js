050eb4cd2a9a7731da844df388d268cec1b02186
GC-Kairos <113116204+GC-Kairos@users.noreply.github.com>
2022-12-01 09:38:48 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,11 +5,11 @@ const CACHE_ESTABLE = 'mi-cache-2';
 self.addEventListener('install', evento => {
     const respCache = caches.open(CACHE_NAME).then( cache => {
         return cache.addAll([
-            '/',
-            'index.html',
-            'css/estilos.css',
-            'app.js',
-            'icons/icon-96x96.png',
+            './',
+            './index.html',
+            './css/estilos.css',
+            './app.js',
+            './icons/icon-96x96.png',
             'https://fonts.googleapis.com/icon?family=Material+Icons',
             'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js'
             
