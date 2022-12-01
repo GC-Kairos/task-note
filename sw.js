cb88754f04fc38c8165c0725af8f75598efb5f80
GC-Kairos <113116204+GC-Kairos@users.noreply.github.com>
2022-12-01 09:14:06 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -31,8 +31,8 @@ self.addEventListener('fetch', evento => {
 
 
 self.addEventListener('install', evento => {
-    const respCache = caches.open(CACHE_ESTABLE).then( cache => {
-        return cache.addAll([
+    const respCache = caches.open(CACHE_ESTABLE).then( cache2 => {
+        return cache2.addAll([
             '/',
             'icons/icon-96x96.png',
             'https://fonts.googleapis.com/icon?family=Material+Icons',
@@ -46,8 +46,8 @@ self.addEventListener('install', evento => {
 
 self.addEventListener('fetch', evento => {
     const respuesta = fetch(  evento.request ).then( respNet => {
-        return caches.open(CACHE_ESTABLE).then( cache => {
-            cache.put( evento.request, respNet.clone() );
+        return caches.open(CACHE_ESTABLE).then( cache2 => {
+            cache2.put( evento.request, respNet.clone() );
             return respNet;
         })
     }).catch( error => {  
