b468dd107bd3214ebb3d8b3841df0e4191179cba
GC-Kairos <113116204+GC-Kairos@users.noreply.github.com>
2022-12-01 09:22:11 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@ self.addEventListener('install', evento => {
             '/',
             'index.html',
             'css/estilos.css',
-            'app.js',
+            'app.js'
           
             
         ])
@@ -36,7 +36,7 @@ self.addEventListener('install', evento => {
             '/',
             'icons/icon-96x96.png',
             'https://fonts.googleapis.com/icon?family=Material+Icons',
-            'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js'
             
         ])
     })
